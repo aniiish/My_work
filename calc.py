67b7532bde42a12e0ae862bca05f93e4862409f3
aniiish <71206148+aniiish@users.noreply.github.com>
2020-09-13 18:18:35 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,26 @@
+print("Welcome to the calculator \n")
+
+
+no1 = int(input("Enter first no :- "))
+no2 = int(input("Enter second no:- "))
+op = input('''choose an operator :-     '+'  For Addition
+                                        '-'  For Substraction
+                                        '*'  For Multiplication 
+                                        '/'  For Division 
+                                         Enter the opertator :-  ''')
+
+
+# checking what is the operator and printing the result 
+ 
+if op=='+':
+    print(no1+no2)
+elif op=='-':
+    if no1>no2:
+        print(no1-no2)
+    else: 
+        print(no2-no1)
+elif op=='/':
+    print(no1/no2)
+    
+else:
+ print(no1*no2)
